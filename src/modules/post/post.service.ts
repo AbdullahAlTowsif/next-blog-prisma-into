d6e6d3d8641415673811e63f7e713dac6c8123f9
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-09-15 20:04:39 +0600

    filtering added to post

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,70 +1,76 @@
 import { Post, Prisma } from "@prisma/client";
 import { prisma } from "../../config/db";
 
-const createPost = async(payload: Prisma.PostCreateInput): Promise<Post> => {
-    console.log("Create Post!!", payload);
-    const createdPost = await prisma.post.create({
-        data: payload,
-        include: {
-            author: {
-                select: {
-                    id: true,
-                    name: true,
-                    email: true
-                }
-            }
-        }
-    })
-    return createdPost
-}
+const createPost = async (payload: Prisma.PostCreateInput): Promise<Post> => {
+  console.log("Create Post!!", payload);
+  const createdPost = await prisma.post.create({
+    data: payload,
+    include: {
+      author: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
+  return createdPost;
+};
 
+const getAllPosts = async ({
+  page = 1,
+  limit = 10,
+  search,
+  isFeatured,
+}: {
+  page?: number;
+  limit?: number;
+  search?: string;
+  isFeatured?: boolean;
+}) => {
+  const skip = (page - 1) * limit;
+  const where: any = {
+    AND: [
+      search && {
+        OR: [
+          { title: { contains: search, mode: "insensitive" } },
+          { content: { contains: search, mode: "insensitive" } },
+        ],
+      },
+      typeof isFeatured === "boolean" && { isFeatured },
+    ].filter(Boolean),
+  };
 
-const getAllPosts = async ({page, limit, search}: {page: number, limit: number, search: string}) => {
-    const skip = (page - 1) * limit;
-    const result = await prisma.post.findMany({
-        skip,
-        take: limit,
-        where: {
-            OR: [
-                {
-                    title: {
-                        contains: search,
-                        mode: "insensitive"
-                    }
-                },
-                {
-                    content: {
-                        contains: search,
-                        mode: "insensitive"
-                    }
-                }
-            ]
-        }
-    });
-    return result;
+  const result = await prisma.post.findMany({
+    skip,
+    take: limit,
+    where,
+  });
+  return result;
 };
 
 const getPostById = async (id: number) => {
-    const result = await prisma.post.findUnique({
-        where: { id },
-        include: { author: true },
-    });
+  const result = await prisma.post.findUnique({
+    where: { id },
+    include: { author: true },
+  });
 
-    return result;
+  return result;
 };
 
 const updatePost = async (id: number, data: Partial<any>) => {
-    return prisma.post.update({ where: { id }, data });
+  return prisma.post.update({ where: { id }, data });
 };
 
 const deletePost = async (id: number) => {
-    return prisma.post.delete({ where: { id } });
+  return prisma.post.delete({ where: { id } });
 };
 
 export const PostService = {
-    createPost,
-    getAllPosts,
-    getPostById,
-    updatePost,
-    deletePost
-}
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+};
